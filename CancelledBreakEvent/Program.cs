12821f8e69718a56850e05715337160bd41097fd
Konstantin Diachenko <drakonard@gmail.com>
2020-04-16 22:24:07 +0300

    Added C# versions of CancelledBreakEvent and CancelledInterruptionEvent.

---
+++ CancelledBreakEvent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace CancelledBreakEvent
+{
+    class Program
+    {
+        static void OnBreak(object sender, ConsoleCancelEventArgs e)
+        {
+            if (e.SpecialKey == ConsoleSpecialKey.ControlBreak)
+            {
+                e.Cancel = true;
+                Console.WriteLine("Break cancelled.");
+            }
+        }
+
+        static void Main()
+        {
+            Console.CancelKeyPress += OnBreak;
+
+            int i = 0;
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
---
+++ CancelledInterruptionEvent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace CancelledInterruptionEvent
+{
+    class Program
+    {
+        static void OnInterruption(object sender, ConsoleCancelEventArgs e)
+        {
+            if (e.SpecialKey == ConsoleSpecialKey.ControlC)
+            {
+                e.Cancel = true;
+                Console.WriteLine("Interruption cancelled.");
+            }
+        }
+
+        static void Main()
+        {
+            Console.CancelKeyPress += OnInterruption;
+
+            int i = 0;
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
